7b2522d4727c902d0307b5b527cc3504f79092dc
cercinc <>
2025-02-10 13:52:31 +0100

    cookie secure to false

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -30,7 +30,7 @@ export async function createSession(userId: number | undefined) {
 
   cookieStore.set("session", session, {
     httpOnly: true,
-    secure: true,
+    secure: false,
     expires: expiresAt,
     sameSite: "lax",
     path: "/",
